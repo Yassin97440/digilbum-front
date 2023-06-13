0af87980a0bbd64be8456b99880d17a583e518f7
Yassin97440 <yassin.abdulla@compufirst.com>
2023-06-13 23:53:41 +0200

    logout + nettoyage

--- stores/AuthStore.js
+++ stores/AuthStore.js
@@ -12,7 +12,7 @@ export const useAuthStore = defineStore("authStore", {
       const { token } = await $fetch(
         "http://localhost:8080/api/v1/auth/register",
         {
-          headers: { "Content-Type": "application/json" },
+          headers: { "Content-Type": "application/json", 'Access-Control-Allow-Origin': '*', },
           method: "POST",
           body: user,
         }
@@ -34,17 +34,20 @@ export const useAuthStore = defineStore("authStore", {
           body: data,
         }
       );
+      const tokenCookie = useCookie('authToken');
+      tokenCookie.value = "Bearer " + token;
 
       this.token = token;
 
     },
-    logout(data) { },
+    async logout() {
+      const { res } = await $fetch("http://localhost:8080/api/v1/auth/logout",
+        {
+          headers: { 'Authorization': useCookie('authToken').value },
+          method: "GET",
+        })
+      const tokenCookie = useCookie('authToken');
+      tokenCookie.value = undefined;
+    },
   },
-  // const { data : register} = await useAsyncData("register",() =>
-  //      $fetch("http://localhost:8080/api/v1/auth/register", {
-  //        headers: { "Content-Type": "application/json" },
-  //        method: "POST",
-  //        body: user,
-  //      })
-  //    );
 });

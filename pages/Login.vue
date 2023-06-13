41aa74bf6a39e3b3deb4200ff9313db8f50fc2c1
Yassin97440 <yassin.abdulla@compufirst.com>
2023-06-13 23:51:54 +0200

    login + ok signup ok à refacto

--- pages/Login.vue
+++ pages/Login.vue
@@ -1,33 +1,32 @@
 <script setup>
 definePageMeta({
     middleware: ["auth"]
-    // or middleware: 'auth'
 })
+
 </script >
 <template>
-    <v-container>
-        <v-form>
+    <v-form>
+        <v-container>
 
             <v-row>
                 <v-col>
-                    <v-text-field v-model="email" label="email" rounded outlined clearable required></v-text-field>
+                    <v-text-field v-model="email" label="ba"></v-text-field>
                 </v-col>
             </v-row>
             <v-row>
                 <v-col>
                     <v-text-field v-model="password" :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-                        :rules="[rules.required, rules.min]" :type="show1 ? 'text' : 'password'" name="input-10-1"
-                        label="Entrez votre mot de passe" hint="👮 Il faut moins 8 caractères 👮" counter
+                        :rules="[rules.required, rules.min]" :type="show1 ? 'text' : 'password'" counter
                         @click:append="show1 = !show1" required></v-text-field>
                 </v-col>
             </v-row>
             <v-row>
                 <v-col>
-                    <v-btn rounded color="primary" dark @click="login"> Enregister </v-btn>
+                    <v-btn rounded color="primary" dark @click="logine"> Enregister </v-btn>
                 </v-col>
             </v-row>
-        </v-form>
-    </v-container>
+        </v-container>
+    </v-form>
 </template>
 <script>
 import { useAuthStore } from "~~/stores/AuthStore";
@@ -47,8 +46,10 @@ export default {
     }),
     methods: {
         ...mapActions(useAuthStore, ["login"]),
-        login() {
-            this.login({ email, password })
+        logine() {
+            console.log("🚀 ~ file: Login.vue:51 ~ login ~ email:", this.email)
+            this.login({ email: this.email, password: this.password })
+            return navigateTo('/')
         }
     }
 }

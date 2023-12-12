59dd69a4ece8599363b575368c1cb0ede8661c55
Yassin97440 <yassin.abdulla@compufirst.com>
2023-12-12 21:11:15 +0100

    login bg et OK

--- pages/Login.vue
+++ pages/Login.vue
@@ -5,7 +5,7 @@ definePageMeta({
 })
 
 </script >
-<template place-content-center>
+<template  >
     <!-- <div class="pt-16 px-16">
 
         <v-form>
@@ -31,13 +31,17 @@ definePageMeta({
             </v-container>
         </v-form>
     </div> -->
-    <div>
 
-        <v-card class="mx-auto pa-12 pb-8" elevation="8" max-width="448" rounded="lg">
+
+    <div class="" style="height: 100vh; display: flex;
+    justify-content: center;
+    align-items: center;">
+
+        <v-card class="mx-auto pa-12 pb-8" elevation="8" max-width="448" rounded="lg" style="width: 50%;">
             <div class="text-subtitle-1 text-medium-emphasis">Account</div>
 
-            <v-text-field density="compact" placeholder="Email address" prepend-inner-icon="mdi-email-outline"
-                variant="outlined"></v-text-field>
+            <v-text-field v-model="email" density="compact" placeholder="Email address"
+                prepend-inner-icon="mdi-email-outline" variant="outlined"></v-text-field>
 
             <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
                 Password
@@ -46,12 +50,13 @@ definePageMeta({
                     Forgot login password?</a>
             </div>
 
-            <v-text-field :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'" :type="visible ? 'text' : 'password'"
-                density="compact" placeholder="Enter your password" prepend-inner-icon="mdi-lock-outline" variant="outlined"
+            <v-text-field v-model="password" :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+                :type="visible ? 'text' : 'password'" density="compact" placeholder="Enter your password"
+                prepend-inner-icon="mdi-lock-outline" variant="outlined"
                 @click:append-inner="visible = !visible"></v-text-field>
 
 
-            <v-btn block class="mb-8" color="blue" size="large" variant="tonal">
+            <v-btn block class="mb-8" color="blue" size="large" variant="tonal" @click="logine">
                 Log In
             </v-btn>
 
@@ -82,9 +87,7 @@ export default {
     methods: {
         ...mapActions(useAuthStore, ["login"]),
         logine() {
-            console.log("🚀 ~ file: Login.vue:51 ~ login ~ email:", this.email)
             this.login({ email: this.email, password: this.password })
-            // return navigateTo('/')
         }
     }
 }

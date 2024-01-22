676b928695878d4480b150e8fc691b82316f850e
Yassin97440 <yassin.abdulla@compufirst.com>
2024-01-22 15:46:13 +0100

    add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     ssr: false,
-    css: ['vuetify/lib/styles/main.sass'],
+    css: ['vuetify/lib/styles/main.sass', '~/assets/css/main.css'],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
     build: {
         transpile: ['vuetify'],
     },

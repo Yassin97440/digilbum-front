170456bbe57238a212f2e2068a4fab4fd8fd9d26
Yassin97440 <yassin.abdulla@compufirst.com>
2024-01-22 17:31:34 +0100

    better login

--- pages/Login.vue
+++ pages/Login.vue
@@ -6,63 +6,29 @@ definePageMeta({
 
 </script >
 <template  >
-    <!-- <div class="pt-16 px-16">
+    <div class="flex justify-center items-center mx-auto h-screen ">
+        <v-card class="mx-auto pa-12 pb-8 w-4/5 sm:w-3/5 md:w-3/5 lg:w-2/5 xl:w-1/3" elevation="8" rounded="lg">
+            <div class="text-subtitle-1 text-medium-emphasis">Identifiant</div>
 
-        <v-form>
-            <v-container fill-height fluid>
-
-                <v-row align="center" justify="center">
-                    <v-col>
-                        <v-text-field v-model="email" label="john.doe@exemple.com"></v-text-field>
-                    </v-col>
-                </v-row>
-                <v-row align="center" justify="center">
-                    <v-col>
-                        <v-text-field v-model="password" :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-                            :rules="[rules.required, rules.min]" :type="show1 ? 'text' : 'password'" counter
-                            @click:append="show1 = !show1" required></v-text-field>
-                    </v-col>
-                </v-row>
-                <v-row align="center" justify="center">
-                    <v-col>
-                        <v-btn rounded color="primary" dark @click="logine"> Enregister </v-btn>
-                    </v-col>
-                </v-row>
-            </v-container>
-        </v-form>
-    </div> -->
-
-
-    <div class="" style="height: 100vh; display: flex;
-    justify-content: center;
-    align-items: center;">
-
-        <v-card class="mx-auto pa-12 pb-8" elevation="8" max-width="448" rounded="lg" style="width: 50%;">
-            <div class="text-subtitle-1 text-medium-emphasis">Account</div>
-
-            <v-text-field v-model="email" density="compact" placeholder="Email address"
+            <v-text-field v-model="email" density="compact" placeholder="Adresse email"
                 prepend-inner-icon="mdi-email-outline" variant="outlined"></v-text-field>
 
             <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
-                Password
-
-                <a class="text-caption text-decoration-none text-blue" href="#" rel="noopener noreferrer" target="_blank">
-                    Forgot login password?</a>
+                Mot de passe
             </div>
 
             <v-text-field v-model="password" :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
-                :type="visible ? 'text' : 'password'" density="compact" placeholder="Enter your password"
+                :type="visible ? 'text' : 'password'" density="compact" placeholder="Mot de passe"
                 prepend-inner-icon="mdi-lock-outline" variant="outlined"
                 @click:append-inner="visible = !visible"></v-text-field>
 
-
             <v-btn block class="mb-8" color="blue" size="large" variant="tonal" @click="logine">
-                Log In
+                Connexion
             </v-btn>
 
             <v-card-text class="text-center">
-                <a class="text-blue text-decoration-none" href="#" rel="noopener noreferrer" target="_blank">
-                    Sign up now <v-icon icon="mdi-chevron-right"></v-icon>
+                <a class="text-blue text-decoration-none" href="/Signup" rel="noopener noreferrer" target="_blank">
+                    Créer un compte <v-icon icon="mdi-chevron-right"></v-icon>
                 </a>
             </v-card-text>
         </v-card>

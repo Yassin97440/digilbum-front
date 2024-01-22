3984531d8caaea3e5196e3a9f302f3edff9a585d
Yassin97440 <yassin.abdulla@compufirst.com>
2024-01-22 15:49:07 +0100

    presque responvice new album

--- components/NewAlbumFormulaire.vue
+++ components/NewAlbumFormulaire.vue
@@ -1,22 +1,22 @@
 <template>
   <v-container>
     <v-row class="d-flex justify-center mb-6">
-      <v-col cols="6" sm="6" md="6">
+      <v-col cols="12" sm="8" md="8">
         <v-text-field v-model="albumName" label="Nom de l'album" rounded outlined clearable></v-text-field>
       </v-col>
     </v-row>
     <v-row class="d-flex justify-center mb-6 gap-5">
-      <v-col cols="12" sm="12" md="3">
+      <v-col cols="12" sm="8" md="8">
         <v-text-field v-model="startDate" type="date" label="Date de début de l'évènement" rounded outlined
           clearable></v-text-field>
       </v-col>
-      <v-col cols="12" sm="12" md="3">
+      <v-col cols="12" sm="8" md="8">
         <v-text-field v-model="endDate" type="date" label="Date de début de l'évènement" rounded outlined
           clearable></v-text-field>
       </v-col>
     </v-row>
     <v-row class="d-flex justify-center mb-6">
-      <v-col cols="6" sm="6" md="6">
+      <v-col cols="12" sm="8" md="8">
         <v-file-input v-model="files" label="Met ton photo terla" counter filled prepend-icon="mdi-camera" multiple
           rounded outlined clearable :show-size="1000">
           <template v-slot:selection="{ index, text }">
@@ -38,12 +38,10 @@
     </v-row>
   </v-container>
 </template>
-<style>
-
-</style>
+<style></style>
 <script>
 import { useAlbumStore } from '~~/stores/AlbumStore';
-import { mapState, mapActions } from "pinia";export default {
+import { mapState, mapActions } from "pinia"; export default {
   data: () => ({
     files: [],
     albumName: "",
@@ -58,7 +56,7 @@ import { mapState, mapActions } from "pinia";export default {
         startDate: this.startDate,
         endDate: this.endDate
       };
-      this.postNewAlbums( {
+      this.postNewAlbums({
         album,
         pictures: this.files,
       });

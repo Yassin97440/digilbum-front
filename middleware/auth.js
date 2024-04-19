6824f41cb2fd8495ef7488a488c5b55ae98cacb8
Yassin97440 <yassin.abdulla@compufirst.com>
2024-04-20 01:05:54 +0200

    middleware login decode for date du token : WIP

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,19 @@
 
+import * as jose from 'jose'
 export default defineNuxtRouteMiddleware((to, from) => {
 
 
     const authToken = useCookie('authToken');
+    // console.log("🚀 ~ defineNuxtRouteMiddleware ~ authToken:", authToken._rawValue)
+    // console.log("joseeee : ", jose.jwtDecrypt(authToken._rawValue));
+    jose.compactDecrypt(authToken._rawValue, { complete: true })
+    // console.log("🚀 ~ defineNuxtRouteMiddleware ~ payload:",
+    //     jose.compactDecrypt(authToken._rawValue, { complete: true })
+    // )
 
     if ((authToken?.value === undefined) && !(to.path === '/Login' || to.path === '/Signup')) {
         return navigateTo('/Login')
     }
-
-    // if ((!authToken?.value === undefined)) {
-    //     console.log("conditon 2")
-    //     return navigateTo(to.path)
-    // }
-
 
 
 })

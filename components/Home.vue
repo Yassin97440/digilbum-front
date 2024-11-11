43ec72a4bf755bb82bfdcdf0b2b2a7fb6aa3b133
Yassin97440 <yassin.abdulla@compufirst.com>
2024-11-11 13:34:54 +0100

    bg le bg

--- components/Home.vue
+++ components/Home.vue
@@ -1,24 +1,23 @@
 <template>
 
-    <v-container class="flex-col grid-rows-5 bg-slate-600 h-full">
+    <v-container class="flex-col grid-rows-5 bg-slate-600  max-h-screen">
         <h1 class="uppercase text-center">Bienvenue sur l'album digital halal</h1>
-        <div class="grid grid-cols-2 gap-4">
+        <div class="grid grid-cols-2 gap-4 ">
 
-            <v-card class="" title="Créer un évnènement/album">
-                <div class="">
+            <v-card class="col-span-1  scale-90 hover:scale-95" title="Créer un évnènement/album">
 
-                    <v-icon>mdi-plus</v-icon>
-                </div>
-                <v-btn variant="tonal">Créer un évnènement/album</v-btn>
-                <v-card-text class="bg-surface-light pt-4">
-                    Vous pouvez créer un évènement et y joindre un ou plusieurs albums photos pour cet évènement. <br>
-                    Par
-                    exemple, si vous avez fait un séjour en Italie, vous pouvez crée
-                </v-card-text>
+
+                <img src="~/assets/add-events-pic-card.jpg" class="object-contain" />
+
             </v-card>
-            <v-card class="mx-auto">
-                <v-icon>mdi-plus</v-icon>
-                <v-btn variant="tonal">AJOUTER UN ALBUM</v-btn>
+            <v-card class="col-span-1 scale-90 hover:scale-95" title="Visionner">
+
+
+                <img src="~/assets/show-events-pic-card.jpg" class="object-contain" />
+                <!-- 
+                <v-card-text class="bg-surface-light pt-4">
+
+                </v-card-text> -->
             </v-card>
         </div>
     </v-container>

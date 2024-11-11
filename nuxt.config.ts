cc026a0bcaa27c4a73060c6000614998978fd8f6
Yassin97440 <yassin.abdulla@compufirst.com>
2024-11-11 13:31:50 +0100

    devtool ok

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,30 +1,39 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    ssr: false,
-    css: ['~/assets/css/main.css'],
-    postcss: {
-        plugins: {
-            tailwindcss: {},
-            autoprefixer: {},
-        },
-    },
-    build: {
-        transpile: ['vuetify'],
-    },
-    modules: [
-        '@invictus.codes/nuxt-vuetify',
-        '@pinia/nuxt',
-        'nuxt-primevue',
-    ],
-    vuetify: {
-        moduleOptions: {
-            styles: true,
-            autoImport: true,
-        }
-    },
-    primevue: {
-        options: {
-            unstyled: true
-        },
-    },
+  ssr: false,
+  css: ['~/assets/css/main.css'],
+
+  postcss: {
+      plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+      },
+  },
+
+  build: {
+      transpile: ['vuetify'],
+  },
+
+  modules: [
+      '@invictus.codes/nuxt-vuetify',
+      '@pinia/nuxt',
+      'nuxt-primevue',
+  ],
+
+  vuetify: {
+      moduleOptions: {
+          styles: true,
+          autoImport: true,
+      }
+  },
+
+  primevue: {
+      options: {
+          unstyled: true
+      },
+  },
+
+  devtools: {
+    enabled: true,
+  },
 })

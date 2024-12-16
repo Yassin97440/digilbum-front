a4b70024d3f9574d3a8fb4955646b7533ae6d3cb
Yassin97440 <yassin.abdulla@compufirst.com>
2024-12-16 21:29:26 +0100

    fix/read JWT payload

--- tools/token/decodeToken.js
+++ tools/token/decodeToken.js
@@ -1,18 +1,31 @@
-// import jwt from 'jsonwebtoken';
+import * as jose from 'jose'
 
-export default function decodeToken() {
-
+export default async function decodeToken() {
     const token = useCookie('authToken');
 
-    // Clé secrète utilisée pour signer le JWT
-    const secretKey = '404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970';
-    // Décodage du JWT
-    // jwt.verify(token, secretKey, (err, decoded) => {
-    //     if (err) {
-    //         console.log('Erreur lors du décodage du token :', err);
-    //     } else {
-    //         // Le payload décrypté du JWT est stocké dans l'objet 'decoded'
-    //         console.log('Payload décrypté du JWT :', decoded);
-    //     }
-    // });
+    if (!token.value) {
+        return null;
+    }
+
+    try {
+        const my_secret = ""
+
+        // Décodage de la clé en Base64
+        const secretBytes = new Uint8Array(
+            atob(my_secret)
+                .split('')
+                .map(char => char.charCodeAt(0))
+        );
+
+        const { payload } = await jose.jwtVerify(token.value, secretBytes, {
+            algorithms: ['HS256']
+        });
+
+
+        // Retourne à la fois le payload et les claims du header
+        return payload;
+    } catch (error) {
+        console.error("Erreur de décodage du token:", error);
+        return null;
+    }
 }
